1baa40d15c616324491cd409a18e1fe997b287d0
RODRIGO LUNA <rodrigo.arreguin@gmail.com>
2020-11-26 08:32:48 +0000

    Simplify axios promise

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
 const axios = require('axios')
 
-let fuckingData = null
-let num = 210
+let num = 202
 
 const tryUntilSuccess = (index) => {
     return new Promise((resolve) => {
         console.log(index);
-
         axios.get('https://jsonplaceholder.typicode.com/todos/' + index)
-              .then(res => { 
-                fuckingData = res.data                 
-              })
-              .catch(e => {        
-                if (e.response) {
-                  console.log(e.response.status)
-                } else if (e.request) {
-                  console.log(e.request)
-                } else {
-                  console.error('Error:', e.message);
-                }
+              .then(res => { return resolve(res.data.title) })
+              .catch(e => {       
+                console.log('Failed to fetch query') 
+                return setTimeout(() => resolve(tryUntilSuccess(--num)), 2000);
               })
 
 
-        if (fuckingData === null) {
-            return setTimeout(() => resolve(tryUntilSuccess(--num)), 2000);
-        } else {
-            return resolve()
-        }
     })
 }
 
-tryUntilSuccess(num).then(() => console.log('done'));
+tryUntilSuccess(num).then((data) => { 
+  console.log(data)
+  console.log('done') 
+});
